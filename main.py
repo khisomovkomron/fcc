992ac23f531e444c90d950a51ad8f534acda8210
Komron Khisomov <normok_9595@bk.ru>
2023-05-30 11:54:49 +0500

    pydantic models

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, Body
-
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None 
+
+
 # request Get Method url: '/'
-
-
 @app.get('/')
 async def root():
     return {'message': "hello world"}
@@ -15,6 +22,7 @@ async def get_posts():
     return {'data':'This is your posts'}
 
 @app.post('/createpost')
-async def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"message": f"title {payload['title']} content {payload['content']}"}
+async def create_post(new_post: Post):
+    print(new_post.published)
+    print(new_post.dict())
+    return new_post
