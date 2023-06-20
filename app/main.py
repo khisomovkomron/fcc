39ebf4f4e606d004a7d6fe3fcecc616cd4e9e376
Komron Khisomov <normok_9595@bk.ru>
2023-06-20 12:13:34 +0500

    add Cross origin resource sharing (CORS)

--- app/main.py
+++ app/main.py
@@ -3,10 +3,22 @@ from . import models
 from .database import engine
 from .routers import auth, post, user, vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*'] 
+
+)
         
 app.include_router(post.router)
 app.include_router(user.router)
