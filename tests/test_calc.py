334aff610d2442675fa860b6810c51007b3e4a58
Komron Khisomov <normok_9595@bk.ru>
2023-06-26 11:18:42 +0500

    pytest parametrize, fixture, exceptions

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -21,12 +21,51 @@ def test_multiply():
 def test_divide():
     assert divide(20, 5) == 4
 
+@pytest.fixture
+def zero_bank_account():
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(starting_balance=50)
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(starting_balance=50)
+
+
+def test_bank_set_initial_amount(bank_account):
 
     assert bank_account.balance == 50
 
-def test_bank_default_amount():
-    bank_amount = BankAccount()
-    assert bank_amount.balance == 0 
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0 
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(20)
+
+    assert bank_account.balance == 30
+
+def test_deposit(bank_account):
+    bank_account.deposit(20)
+
+    assert bank_account.balance == 70
+
+def test_collect_interest(bank_account):
+    bank_account.collect_interest()
+
+    assert int(bank_account.balance) == 55
+
+
+@pytest.mark.parametrize("deposited, withdrawed, expected", [
+    (200, 100, 100),
+    (50, 10, 40),
+    (1200, 200, 1000),
+])
+def test_bank_transaction(zero_bank_account, deposited, withdrawed, expected ):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrawed)
+
+    assert zero_bank_account.balance == expected
+
+def test_insufficient_funds(bank_account):
+    with pytest.raises(Exception):
+        bank_account.withdraw(200)
+
