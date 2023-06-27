09ae84fb657bcb1e57f85562bede6113e8a7d8b2
Komron Khisomov <normok_9595@bk.ru>
2023-06-27 11:23:26 +0500

    restructure code

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,47 +1,5 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db, Base
-import pytest
-from alembic import command
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-@pytest.fixture()
-def session():
-    Base.metadata.drop_all(bind=engine)
-
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@pytest.fixture()
-def client(session):
-    def override_get_db():   
-
-        db = TestingSessionLocal()
-        try:
-            yield session
-        finally:
-            session.close()
-    # command.upgrade('head')
-    app.dependency_overrides[get_db] = override_get_db
-
-    yield TestClient(app)
-    # command.downgrade('base')
-
+from .database import session, client
 
 
 def test_root(client):
@@ -60,7 +18,6 @@ def test_create_user(client):
     assert new_user.email == "komron@gmail.com"
     assert res.status_code == 201
     
-
 
 # pytest flags
 
