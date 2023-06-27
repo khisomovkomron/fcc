a6b4639dbaa5becae5e64900415951c9035cb3a4
Komron Khisomov <normok_9595@bk.ru>
2023-06-27 11:21:09 +0500

    add session & client fixture

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.database import get_db, Base
 import pytest
+from alembic import command
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
@@ -14,22 +15,34 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
 
-def override_get_db():   
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
 
+    Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-app.dependency_overrides[get_db] = override_get_db
-
-
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def override_get_db():   
+
+        db = TestingSessionLocal()
+        try:
+            yield session
+        finally:
+            session.close()
+    # command.upgrade('head')
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
+    # command.downgrade('base')
+
+
 
 def test_root(client):
 
