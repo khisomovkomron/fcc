0e95a07dde07dd71613f7a4e8beff850983c2d18
Komron Khisomov <normok_9595@bk.ru>
2023-06-30 11:24:13 +0500

    tests for post

--- tests/test_post.py
+++ tests/test_post.py
@@ -9,7 +9,31 @@ def test_get_all_posts(authorized_client, test_posts):
         return schemas.PostOut(**post)
     posts_map = map(validate, res.json())
     posts_list = list(posts_map)
-    print(res.json())
+
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
-    assert posts_list[0].Post.id  == test_posts[0].id
+    # assert posts_list[0].Post.id  == test_posts[0].id
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get('/posts/')
+
+    assert res.status_code == 401
+
+def test_unauthorized_user_get_one_posts(client, test_posts):
+    res = client.get(f'/posts/{test_posts[0].id}')
+
+    assert res.status_code == 401
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f'/posts/9999')
+
+    assert res.status_code == 404
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f'/posts/{test_posts[0].id}')
+    print(res.json())
+    post = schemas.PostOut(**res.json())
+
+    assert post.Post.id == test_posts[0].id 
+    assert post.Post.content == test_posts[0].content
